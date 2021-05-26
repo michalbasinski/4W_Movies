cf5d911c090db860692e1fa35a793534b8dbf9b6
Michał Basiński <m.basinski@finanteq.com>
2021-05-26 08:41:39 +0200

    adding swagger to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,8 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation ("io.mockk:mockk:1.11.0")
-
+	implementation ("io.springfox:springfox-swagger2:2.9.2")
+	implementation ("io.springfox:springfox-swagger-ui:2.9.2")
 }
 
 tasks.withType<KotlinCompile> {
